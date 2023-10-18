e13f0cbad89350e0177b47eedcb1686891bdf40e
Reneh7 <renehm2001@gmail.com>
2023-10-18 08:12:04 +0300

    lab27

--- app/src/main/java/com/practice/taskmaster/MainActivity.java
+++ app/src/main/java/com/practice/taskmaster/MainActivity.java
@@ -2,9 +2,12 @@ package com.practice.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,22 +16,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // fetched the add task button using its id
-        Button addTaskButton= findViewById(R.id.addTask);
 
-        // added event listener and inside it we did event handling using intent
+        Button addTaskButton= findViewById(R.id.addTask);
         addTaskButton.setOnClickListener(view -> {
             Intent goToAddTaskFormIntent = new Intent(MainActivity.this, AddTaskActivity.class);
             startActivity(goToAddTaskFormIntent);
         });
 
         //===============================================================
-        Button allTaskButton=findViewById(R.id.allTasks);
 
+        Button allTaskButton=findViewById(R.id.allTasks);
         allTaskButton.setOnClickListener(view -> {
             Intent goToAllTasksIntent = new Intent(MainActivity.this, AllTasksActivity.class);
             startActivity(goToAllTasksIntent);
         });
 
+        //================================================================
+
+        Button settingsPage=findViewById(R.id.settingsButton);
+        settingsPage.setOnClickListener(view -> {
+            Intent goToSettings=new Intent(MainActivity.this,SettingsActivity.class);
+            startActivity(goToSettings);
+        });
+
+        //=================================================================
+
+        TextView user=findViewById(R.id.usernameTextView);
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "DefaultUsername");
+        user.setText(username +"'s Tasks:");
+
+        //==================================================================
+
+        Button firstTask=findViewById(R.id.task1);
+        firstTask.setOnClickListener(view -> {
+            Intent sendTitle=new Intent(MainActivity.this,TaskDetailsActivity.class);
+            sendTitle.putExtra("taskTitle","Cleaning");
+            startActivity(sendTitle);
+        });
+
+
+        Button secondTask=findViewById(R.id.task2);
+        secondTask.setOnClickListener(view -> {
+            Intent sendTitle=new Intent(MainActivity.this,TaskDetailsActivity.class);
+            sendTitle.putExtra("taskTitle","Studying");
+            startActivity(sendTitle);
+        });
+
+
+        Button thirdTask=findViewById(R.id.task3);
+        thirdTask.setOnClickListener(view -> {
+            Intent sendTitle=new Intent(MainActivity.this,TaskDetailsActivity.class);
+            sendTitle.putExtra("taskTitle","Playing");
+            startActivity(sendTitle);
+        });
     }
 }
---
+++ app/src/main/java/com/practice/taskmaster/SettingsActivity.java
@@ -0,0 +1,48 @@
+package com.practice.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        EditText usernameEditText = findViewById(R.id.username);
+
+        Button saveUsername= findViewById(R.id.button);
+        saveUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameEditText.getText().toString();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("username", username);
+                editor.apply();
+                Toast.makeText(SettingsActivity.this, "UserName Changed!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+        Button settingsBackButton=findViewById(R.id.settingsBackButton);
+        settingsBackButton.setOnClickListener(view -> {
+            Intent backToHomeFromSettings=new Intent(SettingsActivity.this,MainActivity.class);
+            startActivity(backToHomeFromSettings);
+        });
+
+
+
+    }
+}
